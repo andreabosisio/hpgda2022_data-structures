ae0939ae466f103e9d6dc14364961a7c2077ea8a
andreabosisio <andrea2.bosisio@mail.polimi.it>
2022-06-22 23:12:49 +0200

    updated .sh files (typos)

--- runDota.sh
+++ runDota.sh
@@ -1,5 +1,5 @@
 # Run 20 iterations on dota-league, with src vertex 55712
-# Save results in dota-league.csv 
+# Save results in results_dota-league.csv 
 echo "Benchmarking dota-league graph (20 iterations)"
-echo "Src_Vertex,PopulateTime(ms),MemUsage(MB),BFSTime(ms),BFSSum,DFSTime(ms),DFSSum" > dota-league.csv 
-bin/exe eval_graphs/dota-league/dota-league 55712 20 -U >> dota-league.csv
+echo "Src_Vertex,PopulateTime(ms),MemUsage(MB),BFSTime(ms),BFSSum,DFSTime(ms),DFSSum" > results_dota-league.csv 
+bin/exe eval_graphs/dota-league/dota-league 55712 20 -U >> results_dota-league.csv
--- runWiki.sh
+++ runWiki.sh
@@ -1,5 +1,5 @@
 # Run 20 iterations on wiki-Talk, with src vertex 2
-# Save results in wiki-Talk.csv 
+# Save results in results_wiki-Talk.csv 
 echo "Benchmarking wiki-Talk graph (20 iterations)"
-echo "Src_Vertex,PopulateTime(ms),MemUsage(MB),BFSTime(ms),BFSSum,DFSTime(ms),DFSSum" > wiki-Talk.csv 
-bin/exe eval_graphs/wiki-Talk/wiki-Talk 2 20 >> wiki-Talk.csv
+echo "Src_Vertex,PopulateTime(ms),MemUsage(MB),BFSTime(ms),BFSSum,DFSTime(ms),DFSSum" > results_wiki-Talk.csv 
+bin/exe eval_graphs/wiki-Talk/wiki-Talk 2 20 >> results_wiki-Talk.csv
